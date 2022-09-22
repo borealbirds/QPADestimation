87ed34aa7cdecdae299f588dce810e07254fbaf0
ecknight <ellyknight@gmail.com>
2022-09-22 15:37:13 -0600

    Complete output

--- script/02.CleanVisitData.R
+++ script/02.CleanVisitData.R
@@ -8,6 +8,7 @@ library(tidyverse) #basic data wrangling
 library(lubridate) #date manipulation
 library(suncalc) #sunrise time retrieval
 library(sf) #spatial manipulation
+library(terra) #raster creation
 library(downloader) #download spatial data
 library(data.table) #to collapse rgee output
 
@@ -17,18 +18,24 @@ library(data.table) #to collapse rgee output
 #NOTE: NEED TO DECIDE HOW TO CREATE ALPHA CODE OR USE HISTORIC? IF HISTORIC, DOWNLOAD FROM REPO####
 #NOTE: HOW TO DEAL WITH SURVEYS TOO FAR NORTH FOR SUNRISE????####
 #Peter just had NAs in his data, so presumably removed them
-summary(e$dat %>% dplyr::filter(Y>66) %>% dplyr::select(TSSR, srise, Y))
 #NOTE: DATA PRIOR TO 2000 USES 2000 VALUE FOR CANOPY COVER. PONDER####
+#NOTE: DATA SOUTH OF HERMOSILLA JUST USES COPERNICUS 2015 CLASS####
+#NOTE: COME BACK TO FILTERING DATASETS FOR ARU DATA####
+#NOTE: FILL GREENDAY NAs WITH VALUE FROM ANOTHER YEAR####
 
-#1. Load previous dataset----
+#Load previous dataset to look at it----
 #load into separate environment to avoid overwriting things
 e <- new.env()
 load("data/new_offset_data_package_2017-03-01.Rdata", envir = e)
+#look at data structure
+str(e$dat)
 
-#2. Load in new dataset----
+#1. Load in new dataset----
 load("data/wildtrax_data_2022-07-24.Rdata")
 
-#3. Subset to visits & filter----
+#2. Subset to visits & filter----
+#Remove surveys with no location
+#Remove surveys with no temporal or spatial structure within a visit
 dat <- raw %>%
     dplyr::select(organization, project, location, latitude, longitude, observer, date, recording_date, distanceMethod, durationMethod) %>%
     unique() %>%
@@ -36,18 +43,16 @@ dat <- raw %>%
                   latitude > 0,
                   !(distanceMethod %in% c("0m-INF", NA, "0m-INF-ARU", "UNKNOWN") &
                         durationMethod %in% c("0-3min", "0-5min", "0-10min", "0-2min", "0-20min", "UNKNOWN", NA)))
-#Remove surveys with no location
-#Remove surveys with no temporal or spatial structure within a visit
 
 #Take subsample for testing
 #dat <- sample_n(dat, 1000)
 
-#4. Determine survey type---
+#3. Determine survey type---
 type <- dat %>%
     mutate(type = case_when(!is.na(date) ~ "human",
                             !is.na(recording_date) ~ "aru"))
 
-#5. Wrangle temporal variables----
+#4. Wrangle temporal variables----
 temporal <- type %>%
     mutate(datetime = case_when(type=="human" ~ ymd_hms(date),
                                 type=="aru" ~ ymd_hms(recording_date)),
@@ -56,7 +61,7 @@ temporal <- type %>%
            start_time = hour(datetime) + minute(datetime)/60) %>%
     dplyr::filter(year > 1900)
 
-#6. Get sunrise time----
+#5. Get sunrise time----
 sun <- temporal %>%
     mutate(date = ymd(str_sub(datetime, 1, 10))) %>%
     rename(lat = latitude, lon = longitude)
@@ -64,41 +69,72 @@ sun <- temporal %>%
 sun$sunrise <- getSunlightTimes(data=sun, keep="sunrise")$sunrise
 sun$tssr <- as.numeric(difftime(sun$datetime, sun$sunrise), units="hours")
 
-#7. Lookup method codes----
+#6. Lookup method codes----
 
 
 dist_codes <- read.csv("data/distance_band_codes.csv")
 dur_codes <- read.csv("data/durint.csv")
 
-#8. Get region----
+#7. Get region----
 
-#8a. Download data
+#7a. Download data
 # temp <- tempfile()
 # download("https://birdscanada.org/download/gislab/bcr_terrestrial_shape.zip", temp)
 # unzip(zipfile=temp, exdir="gis")
 # unlink(temp)
 
-#8b.Read in & wrangle shapefile
-bcr <- read_sf("gis/BCR_Terrestrial/BCR_Terrestrial_master.shp") %>%
-    dplyr::select(BCR, BCRNAME, PROVINCE_S, COUNTRY) %>%
-    rename(bcr = BCR, bcrname = BCRNAME, province = PROVINCE_S, country = COUNTRY) %>%
-    st_transform(crs=4326) %>%
-    st_make_valid()
+#7b.Read in & wrangle shapefile
+bcrshp <- read_sf("gis/BCR_Terrestrial/BCR_Terrestrial_master.shp") %>%
+    dplyr::select(BCR, PROVINCE_S, COUNTRY) %>%
+    rename(bcr = BCR, province = PROVINCE_S, country=COUNTRY) %>%
+    dplyr::filter(country %in% c("USA", "CANADA")) %>%
+    mutate(countryid = as.numeric(as.factor(country)),
+           provinceid = as.numeric(as.factor(province))) %>%
+    st_transform(crs=3857) %>%
+    st_make_valid() %>%
+    vect()
 
-#8c. Intersect with visits
-region <- sun %>%
+#7c. Create rasters (much faster than from polygon)
+r <- rast(ext(bcrshp), resolution=1000)
+
+bcr <- rasterize(x=bcrshp, y=r, field="bcr")
+province <- rasterize(x=bcrshp, y=r, field="provinceid")
+country <- rasterize(x=bcrshp, y=r, field="countryid")
+
+#7d. Extract values
+bcrid <- sun %>%
     st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
-    st_intersection(bcr)
+    st_transform(crs=3857) %>%
+    vect() %>%
+    terra::extract(x=bcr)
 
-#9. Get covariates----
-#ROAD: this is all 0s in Peter's dataset
-#TREE: % canopy cover? USE HANSEN
-#TREE3: open < 0.25, sparse = 0.25:0.6, dense > 0.6
-#hab_NALC1: combination of tree3 to conif, decid, mixed, wet
-#hab_NALC2: conif, agr, barren, decid, devel, grass, mixed, shrub, wet: ANNUAL FROM HERMOSILLA?
-#SPRNG: day of local spring (numeric from 16.5 to 182.3)
-#TSLS: this is days since local spring/365
-#DD5
+provinceid <- sun %>%
+    st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
+    st_transform(crs=3857) %>%
+    vect() %>%
+    terra::extract(x=province)
+
+countryid <- sun %>%
+    st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
+    st_transform(crs=3857) %>%
+    vect() %>%
+    terra::extract(x=country)
+
+#7e. Create lookup table to join back ids
+bcrtbl <- st_as_sf(bcrshp) %>%
+    as.data.frame() %>%
+    dplyr::select(-geometry) %>%
+    unique()
+
+#7f. Put together
+region <- sun %>%
+    cbind(bcrid %>% dplyr::select(bcr)) %>%
+    cbind(provinceid %>% dplyr::select(provinceid)) %>%
+    cbind(countryid %>% dplyr::select(countryid)) %>%
+    left_join(bcrtbl) %>%
+    dplyr::select(-provinceid, -countryid)
+
+#8. Get covariates----
 
 #9a. Initialize rgee
 library(rgee)
@@ -111,84 +147,99 @@ years <- sort(unique(region$year))
 out.list <- list()
 for(i in 1:length(years)){
 
-    #9c. Subset to year and format for rgee
+    #9c. Filter to year and subset into chunks of 5000
     dat <- region %>%
-        dplyr::filter(year==years[i])
+        st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
+        dplyr::filter(year==years[i]) %>%
+        mutate(loop = ceiling(row_number()/5000))
 
-    dat.ee <- dat %>%
-        dplyr::select(geometry) %>%
-        sf_as_ee()
+    dat.out <- data.frame()
+    #9d. Set up loop
+    for(j in 1:max(dat$loop)){
 
-    #9d. Get Hansen dataset for canopy cover
-    tree <- ee$Image('UMD/hansen/global_forest_change_2021_v1_9')
+        #9d. Format for rgee
+        dat.j <- dat %>%
+            dplyr::filter(loop==j)
 
-    dat.tree <- ee_extract(
-        x=tree,
-        y=dat.ee,
-        scale=100,
-        sf=FALSE
-    ) %>%
-        dplyr::select(-first_b30, -first_b40, -first_b50, -first_b70, -last_b30, -last_b40, -last_b50, -last_b70, -datamask)
+        dat.ee <- dat.j %>%
+            dplyr::select(geometry) %>%
+            sf_as_ee()
 
-    #9e. Get Hermosilla landcover
-    if(years[i] < 1984){year.i <- 1984}
-    if(years[i] >= 2001 & years[i] <= 2019){year.i <- years[i]}
-    if(years[i] > 2019){year.i <- 2019}
+        #9d. Get Hansen dataset for canopy cover
+        tree <- ee$Image('UMD/hansen/global_forest_change_2021_v1_9')
 
-    start<-paste0(year.i, "-01-01")
-    end<-paste0(year.i,"-12-31")
+        dat.tree <- ee_extract(
+            x=tree,
+            y=dat.ee,
+            scale=100,
+            sf=FALSE
+        ) %>%
+            dplyr::select(-first_b30, -first_b40, -first_b50, -first_b70, -last_b30, -last_b40, -last_b50, -last_b70, -datamask)
 
-    lc <- ee$ImageCollection('projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2')$filterDate(start, end)
+        #9e. Get Hermosilla landcover
+        yeartbl <- data.frame(year = years,
+                              year.i = c(1988, 1990, 1990, 1993, 1993, 1995, 1995, 1997, 1997, 1999, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2007, 2008, 2010, 2010, 2010, 2010, 2015, 2015, 2016, 2018, 2018, 2019, 2019))
+        year.i <- yeartbl$year.i[i]
 
-    dat.lc <- ee_extract(
-        x=lc,
-        y=dat.ee,
-        scale=100,
-        sf=FALSE
-    )
-    colnames(dat.lc) <- "hermosilla"
+        start<-paste0(year.i, "-01-01")
+        end<-paste0(year.i,"-12-31")
 
-    #9f. Get 2015 copernicus to fill gaps
-    start <- "2015-01-01"
-    end <- "2015-12-31"
+        lc <- ee$ImageCollection('projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2')$filterDate(start, end)
 
-    cop <- ee$ImageCollection('COPERNICUS/Landcover/100m/Proba-V-C3/Global')$filterDate(start, end)
+        dat.lc <- ee_extract(
+            x=lc,
+            y=dat.ee,
+            scale=100,
+            sf=FALSE
+        )
+        colnames(dat.lc) <- "hermosilla"
 
-    dat.cop <- ee_extract(
-        x=cop,
-        y=dat.ee,
-        scale=100,
-        sf=FALSE
-    )
-    colnames(dat.cop) <- c("bare", "crop", "density", "copernicus", "prob", "forest", "grass", "moss", "shrub", "snow", "tree", "urban", "permanentwater", "seasonalwater")
+        #9f. Get 2015 copernicus to fill gaps
+        start <- "2015-01-01"
+        end <- "2015-12-31"
+
+        cop <- ee$ImageCollection('COPERNICUS/Landcover/100m/Proba-V-C3/Global')$filterDate(start, end)
+
+        dat.cop <- ee_extract(
+            x=cop,
+            y=dat.ee,
+            scale=100,
+            sf=FALSE
+        )
+        colnames(dat.cop) <- c("bare", "crop", "density", "copernicus", "prob", "forest", "grass", "moss", "shrub", "snow", "tree", "urban", "permanentwater", "seasonalwater")
 
 
-    #9g. Get modis greenup day
-    if(years[i] < 2001){year.i <- 2001}
-    if(years[i] >= 2001 & years[i] <= 2019){year.i <- years[i]}
-    if(years[i] > 2019){year.i <-2019}
+        #9g. Get modis greenup day
+        if(years[i] < 2001){year.i <- 2001}
+        if(years[i] >= 2001 & years[i] <= 2019){year.i <- years[i]}
+        if(years[i] > 2019){year.i <-2019}
 
-    start<-paste0(year.i, "-01-01")
-    end<-paste0(year.i,"-12-31")
+        start<-paste0(year.i, "-01-01")
+        end<-paste0(year.i,"-12-31")
 
-    green <- ee$ImageCollection('MODIS/006/MCD12Q2')$select('Greenup_1')$filterDate(start, end)
+        green <- ee$ImageCollection('MODIS/006/MCD12Q2')$select('Greenup_1')$filterDate(start, end)
 
-    dat.green <- ee_extract(
-        x=green,
-        y=dat.ee,
-        scale=100,
-        sf=FALSE
-    )
-    colnames(dat.green) <- "green"
-    dat.green$greenday <- dat.green$green - as.numeric(ymd(start))
+        dat.green <- ee_extract(
+            x=green,
+            y=dat.ee,
+            scale=100,
+            sf=FALSE
+        )
+        colnames(dat.green) <- "green"
+        dat.green$greenday <- dat.green$green - as.numeric(ymd(start))
 
-    #Put everything together
-    out.list[[i]] <- data.frame(st_coordinates(dat)) %>%
-        rename(lat = Y, lon = X) %>%
-        cbind(data.frame(dat) %>%
-                  dplyr::select(-geometry)) %>%
-        cbind(dat.tree, dat.lc, dat.cop, dat.green)
+        #Put everything together
+        dat.out <- data.frame(st_coordinates(dat.j)) %>%
+            rename(lat = Y, lon = X) %>%
+            cbind(data.frame(dat.j) %>%
+                      dplyr::select(-geometry)) %>%
+            cbind(dat.tree, dat.lc, dat.cop, dat.green) %>%
+            rbind(dat.out)
 
+    }
+
+    #Save to list
+    out.list[[i]] <- dat.out
 
     print(paste0("Finished year ", years[i], ": ", i, " of ", length(years), " years"))
 
