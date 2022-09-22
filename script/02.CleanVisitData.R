a1eb60b7f9c1bd7bb09b0e04e6948e051515f3b0
ecknight <ellyknight@gmail.com>
2022-09-22 15:58:24 -0600

    Tidy

--- script/02.CleanVisitData.R
+++ script/02.CleanVisitData.R
@@ -2,16 +2,8 @@
 # title: "QPAD estimation - clean visit data"
 # author: "Elly Knight"
 # created: "July 24, 2022"
+# updated: "September 22, 2022"
 # ---
-
-library(tidyverse) #basic data wrangling
-library(lubridate) #date manipulation
-library(suncalc) #sunrise time retrieval
-library(sf) #spatial manipulation
-library(terra) #raster creation
-library(downloader) #download spatial data
-library(data.table) #to collapse rgee output
-
 
 #NOTE: INVESTIGATE MISSING LAT LONS - email sent####
 #NOTE: NEED TO GET ARU MODEL - waiting for next WT update Sept 28####
@@ -22,6 +14,14 @@ library(data.table) #to collapse rgee output
 #NOTE: DATA SOUTH OF HERMOSILLA JUST USES COPERNICUS 2015 CLASS####
 #NOTE: COME BACK TO FILTERING DATASETS FOR ARU DATA####
 #NOTE: FILL GREENDAY NAs WITH VALUE FROM ANOTHER YEAR####
+
+library(tidyverse) #basic data wrangling
+library(lubridate) #date manipulation
+library(suncalc) #sunrise time retrieval
+library(sf) #spatial manipulation
+library(terra) #raster creation
+library(downloader) #download spatial data
+library(data.table) #to collapse rgee output
 
 #Load previous dataset to look at it----
 #load into separate environment to avoid overwriting things
@@ -52,7 +52,7 @@ type <- dat %>%
     mutate(type = case_when(!is.na(date) ~ "human",
                             !is.na(recording_date) ~ "aru"))
 
-#4. Wrangle temporal variables----
+#3. Wrangle temporal variables----
 temporal <- type %>%
     mutate(datetime = case_when(type=="human" ~ ymd_hms(date),
                                 type=="aru" ~ ymd_hms(recording_date)),
@@ -61,7 +61,7 @@ temporal <- type %>%
            start_time = hour(datetime) + minute(datetime)/60) %>%
     dplyr::filter(year > 1900)
 
-#5. Get sunrise time----
+#4. Get sunrise time----
 sun <- temporal %>%
     mutate(date = ymd(str_sub(datetime, 1, 10))) %>%
     rename(lat = latitude, lon = longitude)
@@ -69,21 +69,21 @@ sun <- temporal %>%
 sun$sunrise <- getSunlightTimes(data=sun, keep="sunrise")$sunrise
 sun$tssr <- as.numeric(difftime(sun$datetime, sun$sunrise), units="hours")
 
-#6. Lookup method codes----
+#5. Lookup method codes----
 
 
 dist_codes <- read.csv("data/distance_band_codes.csv")
 dur_codes <- read.csv("data/durint.csv")
 
-#7. Get region----
+#6. Get region----
 
-#7a. Download data
+#6a. Download data
 # temp <- tempfile()
 # download("https://birdscanada.org/download/gislab/bcr_terrestrial_shape.zip", temp)
 # unzip(zipfile=temp, exdir="gis")
 # unlink(temp)
 
-#7b.Read in & wrangle shapefile
+#6b.Read in & wrangle shapefile
 bcrshp <- read_sf("gis/BCR_Terrestrial/BCR_Terrestrial_master.shp") %>%
     dplyr::select(BCR, PROVINCE_S, COUNTRY) %>%
     rename(bcr = BCR, province = PROVINCE_S, country=COUNTRY) %>%
@@ -94,14 +94,14 @@ bcrshp <- read_sf("gis/BCR_Terrestrial/BCR_Terrestrial_master.shp") %>%
     st_make_valid() %>%
     vect()
 
-#7c. Create rasters (much faster than from polygon)
+#6c. Create rasters (much faster than from polygon)
 r <- rast(ext(bcrshp), resolution=1000)
 
 bcr <- rasterize(x=bcrshp, y=r, field="bcr")
 province <- rasterize(x=bcrshp, y=r, field="provinceid")
 country <- rasterize(x=bcrshp, y=r, field="countryid")
 
-#7d. Extract values
+#6d. Extract values
 bcrid <- sun %>%
     st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
     st_transform(crs=3857) %>%
@@ -120,13 +120,13 @@ countryid <- sun %>%
     vect() %>%
     terra::extract(x=country)
 
-#7e. Create lookup table to join back ids
+#6e. Create lookup table to join back ids
 bcrtbl <- st_as_sf(bcrshp) %>%
     as.data.frame() %>%
     dplyr::select(-geometry) %>%
     unique()
 
-#7f. Put together
+#6f. Put together
 region <- sun %>%
     cbind(bcrid %>% dplyr::select(bcr)) %>%
     cbind(provinceid %>% dplyr::select(provinceid)) %>%
@@ -134,30 +134,30 @@ region <- sun %>%
     left_join(bcrtbl) %>%
     dplyr::select(-provinceid, -countryid)
 
-#8. Get covariates----
+#7. Get covariates----
 
-#9a. Initialize rgee
+#7a. Initialize rgee
 library(rgee)
 ee_Initialize()
 ee_check()
 
-#9b. Set up to loop through data years
+#7b. Set up to loop through data years
 years <- sort(unique(region$year))
 
 out.list <- list()
 for(i in 1:length(years)){
 
-    #9c. Filter to year and subset into chunks of 5000
+    #7c. Filter to year and subset into chunks of 5000
     dat <- region %>%
         st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
         dplyr::filter(year==years[i]) %>%
         mutate(loop = ceiling(row_number()/5000))
 
     dat.out <- data.frame()
-    #9d. Set up loop
+    #7d. Set up loop
     for(j in 1:max(dat$loop)){
 
-        #9d. Format for rgee
+        #7e. Format for rgee
         dat.j <- dat %>%
             dplyr::filter(loop==j)
 
@@ -165,7 +165,7 @@ for(i in 1:length(years)){
             dplyr::select(geometry) %>%
             sf_as_ee()
 
-        #9d. Get Hansen dataset for canopy cover
+        #7f. Get Hansen dataset for canopy cover
         tree <- ee$Image('UMD/hansen/global_forest_change_2021_v1_9')
 
         dat.tree <- ee_extract(
@@ -176,7 +176,7 @@ for(i in 1:length(years)){
         ) %>%
             dplyr::select(-first_b30, -first_b40, -first_b50, -first_b70, -last_b30, -last_b40, -last_b50, -last_b70, -datamask)
 
-        #9e. Get Hermosilla landcover
+        #7g. Get Hermosilla landcover
         yeartbl <- data.frame(year = years,
                               year.i = c(1988, 1990, 1990, 1993, 1993, 1995, 1995, 1997, 1997, 1999, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2007, 2008, 2010, 2010, 2010, 2010, 2015, 2015, 2016, 2018, 2018, 2019, 2019))
         year.i <- yeartbl$year.i[i]
@@ -194,7 +194,7 @@ for(i in 1:length(years)){
         )
         colnames(dat.lc) <- "hermosilla"
 
-        #9f. Get 2015 copernicus to fill gaps
+        #7h. Get 2015 copernicus to fill gaps
         start <- "2015-01-01"
         end <- "2015-12-31"
 
@@ -209,7 +209,7 @@ for(i in 1:length(years)){
         colnames(dat.cop) <- c("bare", "crop", "density", "copernicus", "prob", "forest", "grass", "moss", "shrub", "snow", "tree", "urban", "permanentwater", "seasonalwater")
 
 
-        #9g. Get modis greenup day
+        #7i. Get modis greenup day
         if(years[i] < 2001){year.i <- 2001}
         if(years[i] >= 2001 & years[i] <= 2019){year.i <- years[i]}
         if(years[i] > 2019){year.i <-2019}
@@ -228,7 +228,7 @@ for(i in 1:length(years)){
         colnames(dat.green) <- "green"
         dat.green$greenday <- dat.green$green - as.numeric(ymd(start))
 
-        #Put everything together
+        #7j.Put everything together
         dat.out <- data.frame(st_coordinates(dat.j)) %>%
             rename(lat = Y, lon = X) %>%
             cbind(data.frame(dat.j) %>%
@@ -238,14 +238,14 @@ for(i in 1:length(years)){
 
     }
 
-    #Save to list
+    #7k. Save to list
     out.list[[i]] <- dat.out
 
     print(paste0("Finished year ", years[i], ": ", i, " of ", length(years), " years"))
 
 }
 
-#10. Create lookup tables for landcover classes----
+#8. Create lookup tables for landcover classes----
 #hermosilla codes
 hermosillacodes <- data.frame(hermosilla = c(0, 20, 31, 32, 33, 40, 50, 80, 81, 100, 210, 220, 230),
                               hermosilladesc = c("unclassified", "water", "snow", "rock", "barren", "bryoid", "shrub", "wetland", "wetlandtreed", "herb", "conifer", "deciduous", "mixedwood"),
@@ -258,7 +258,7 @@ copernicuscodes <- data.frame(copernicus = c(0, 20, 30, 40, 50, 60, 70, 80, 90, 
                               copernicus1 = c(NA, "open", "open", "open", "open", "open", "open", "open", "open", "open", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "treed", "open"),
                               copernicus2 = c(NA, "open", "open", "open", "open", "open", "open", "open", "wetland", "open", "conifer", "deciduous", "conifer", "deciduous", "mixed", "mixed", "conifer", "deciduous", "conifer", "deciduous", "mixed", "mixed", "open"))
 
-#11. Tidy and save----
+#9. Tidy and save----
 visit <- rbindlist(out.list, fill=TRUE) %>%
     data.frame() %>%
     mutate(lossyear = ifelse(is.na(lossyear), 0, lossyear+2000),
