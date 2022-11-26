ed7611209133dd9005c2aded6ef959d3ee02ca55
ecknight <ellyknight@gmail.com>
2022-11-26 10:30:43 -0700

    Include ARUs

--- script/03.CleanBirdData.R
+++ script/03.CleanBirdData.R
@@ -8,88 +8,71 @@
 library(tidyverse) #basic data wrangling
 library(lubridate) #date and time wrangling
 
+root <- "G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/"
+
 #1. Load in use  & visit dataset----
-load("G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/qpadv4_dat_2022-11-06.Rdata")
-load("G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/qpadv4_visit_2022-11-06.Rdata")
+load(file.path(root,"qpadv4_raw.Rdata"))
+load(file.path(root,"qpadv4_visit.Rdata"))
 
 #2. Filter, tidy, create foreign key for visit table----
 #Ensure there's visit data
 #Remove UNSPs
+#Remove records without abundance
 dat <- use %>% 
-    mutate(datetime = ymd_hms(date),
-           julian = yday(datetime),
-           id = paste(location, observer, datetime)) %>%
-    rename(lat = latitude, lon = longitude) %>% 
-    dplyr::filter(str_sub(speciesCode, 1, 2)!="UN",
-                  id %in% visit$id)
+    dplyr::filter(str_sub(species, 1, 2)!="UN",
+                  id %in% visit$id,
+                  !is.na(abundance),
+                  !abundance %in% c("CI 1", "CI 2", "CI 3", "N/A", "0", ""))
 
 #2. Filter to just first detection per individual and bin in minutes----
 #bin in 30 s bins for 60 s surveys
+#Fill in distance & duration method from visit object
 first <- dat %>%
-  dplyr::filter(method=="ARU") %>% 
-  group_by(id, organization, project, location, lat, lon, observer, datetime, durationMethod, speciesCode, abundance, individual_appearance_order) %>%
-  mutate(first_tag = min(tag_start_s)) %>%
+  dplyr::filter(sensor=="ARU") %>% 
+  dplyr::select(-distanceMethod, -durationMethod) %>% 
+  left_join(visit %>% 
+              dplyr::select(id, distanceMethod, durationMethod)) %>%
+  group_by(id, source, project, sensor, singlesp, location, buffer, lat, lon, year, date, observer, species, abundance, individual) %>% 
+  mutate(firstTag = min(tagStart)) %>%
   ungroup() %>%
-  dplyr::filter(tag_start_s == first_tag) %>%
-  dplyr::select(-first_tag) %>%
-  dplyr::filter(!abundance %in% c("CI 1", "CI 2", "CI 3", "N/A", "0")) %>%
-  mutate(end=ifelse(durationMethod=="0-0.5-1min", ceiling(tag_start_s/30), ceiling(tag_start_s/60)),
+  dplyr::filter(tagStart == firstTag) %>% 
+  mutate(end=ifelse(durationMethod=="0-0.5-1min", ceiling(tagStart/30), ceiling(tagStart/60)),
          end = ifelse(end==0, 1, end),
          durationInterval = case_when(durationMethod=="0-0.5-1min" & end==1 ~ "0-0.5min",
                                       durationMethod=="0-0.5-1min" & end==2 ~ "0.5-1min",
-                                      !is.na(durationMethod) ~ paste0(end-1, "-", end, "min"))) %>% 
+                                      !is.na(durationMethod) ~ paste0(end-1, "-", end, "min")),
+         distanceBand = "UNKNOWN") %>% 
     dplyr::select(colnames(dat)) %>% 
     rbind(dat %>% 
-            dplyr::filter(method=="PC")) %>%
-  dplyr::select(id, dataset, organization, project, method, location, lat, lon, observer, datetime, distanceMethod, durationMethod, speciesCode, distanceBand, durationInterval, abundance)
+            dplyr::filter(sensor=="PC")) %>%
+  dplyr::select(id, source, project, sensor, singlesp, location, buffer, lat, lon, year, date, observer, distanceMethod, durationMethod, distanceBand, durationInterval, species, abundance)
 
-#3. Replace TMTTs----
-#99 percentile for combination of species & observer
-tmtttbl <- first %>%
-  dplyr::filter(abundance!="TMTT",
-                method=="ARU") %>%
-  group_by(id, observer, speciesCode) %>%
-  summarize(abundance = sum(as.numeric(abundance))) %>%
-  group_by(speciesCode, observer) %>%
-  summarize(q = quantile(abundance, 0.99),
-            nobs = n()) %>%
-  ungroup() %>%
-  mutate(val = ceiling(q))
+#3. Replace TMTTs with predicted abundance----
+tmtt <- read.csv("C:/Users/Elly Knight/Documents/ABMI/Projects/TMTT/data/tmtt_predictions.csv") %>% 
+  rename(species = species_code)
+user <- read.csv("C:/Users/Elly Knight/Documents/ABMI/Projects/TMTT/data/app_user.csv") %>% 
+  rename(observer = user_name) %>% 
+  dplyr::select(observer, user_id)
 
-#Replace observer species combos with less than 20 with generic "observer"
-tmttn <- first %>%
-  dplyr::filter(abundance=="TMTT") %>%
-  group_by(observer, speciesCode) %>%
-  summarize(ntmtt=n()) %>%
-  ungroup() %>%
-  left_join(tmtttbl) %>%
-  dplyr::filter(!is.na(val)) %>%
-  mutate(ptmtt = ntmtt/nobs) %>%
-  mutate(obs = ifelse(ntmtt < 20, "observer", observer))
+bird <- first %>% 
+  dplyr::filter(abundance=="TMTT") %>% 
+  left_join(user) %>% 
+  mutate(user_id = ifelse(is.na(user_id), observer, user_id))%>% 
+  mutate(boot = round(runif(max(row_number()), 1, 100)),
+         species = ifelse(species %in% tmtt$species, species, "species"),
+         user_id = as.integer(ifelse(user_id %in% tmtt$user_id, user_id, 0))) %>% 
+  data.frame() %>% 
+  left_join(tmtt) %>% 
+  mutate(abundance = round(pred)) %>% 
+  dplyr::select(colnames(first)) %>% 
+  rbind(first %>% 
+          dplyr::filter(abundance!="TMTT") %>% 
+          mutate(abundance = as.numeric(abundance))) 
 
-#Model 99% quantile with random effects for species and observer
-lm.tmtt <- lme4::lmer(val ~ nobs + (1|speciesCode) + (1|obs), data=tmttn)
-
-#Check variance explained by REs
-summary(lm.tmtt)
-
-#Predict to get values to replace TMTTs with
-tmttpred <- data.frame(pred = predict(lm.tmtt)) %>%
-  cbind(data.frame(tmttn)) %>%
-  mutate(predabun = round(pred))
-
-#Replace TMTTs
-bird <- first %>%
-  left_join(tmttpred) %>%
-  mutate(abundance = as.numeric(ifelse(abundance=="TMTT", as.numeric(predabun), abundance)),
-         distanceMethod = NA,
-         distanceBand = NA) %>%
-  dplyr::filter(!is.na(abundance)) %>%
-  dplyr::select(id, dataset, organization, method, project, location, lat, lon, observer, datetime, durationMethod, distanceMethod, speciesCode, durationInterval, distanceBand, abundance)
 
 #3. Add species list----
-species <- read.csv("G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/singing-species.csv") %>%
-    rename(speciesCode = Species_ID)
+species <- read.csv(file.path("singing-species.csv")) %>%
+    rename(species = Species_ID)
 
 #4. Save----
-save(visit, bird, species,  file="G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/qpadv4_clean_2022-11-06.Rdata")
+save(visit, bird, species,  file="G:/.shortcut-targets-by-id/0B1zm_qsix-gPbkpkNGxvaXV0RmM/BAM.SharedDrive/RshProjs/PopnStatus/QPAD/Data/qpadv4_clean.Rdata")
